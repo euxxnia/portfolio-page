5f18d4a62d0b6d128445f88506bbf988ea2db39e
euxxnia <syoomch12@gmail.com>
2026-01-06 23:16:35 +0900

    fix cicd error

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
 import react from '@woohm402/eslint-config-react';
 
 export default [
-  { ignores: ['eslint.config.js', '.yarn'] },
+  {
+    ignores: [
+      'eslint.config.js',
+      '.yarn',
+      'node_modules/**',
+      'dist/**',
+      'dist-ssr/**',
+    ],
+  },
   ...react({
     tsconfigRootDir: import.meta.dirname,
   }),
